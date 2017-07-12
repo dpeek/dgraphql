4b7accc6e1a1c830fc36efccaefacccd64c8eae9
David Peek <mail@dpeek.com>
2017-07-13 00:13:11 +0100

    Cleanup getMutation

--- src/mutate/getMutation.js
+++ src/mutate/getMutation.js
@@ -8,6 +8,13 @@ import { unwrapNonNull } from '../utils'
 import type { GraphQLResolveInfo } from 'graphql'
 import type { Context } from '../client'
 
+type NodeInput = {
+  id?: string,
+  updatedAt: string,
+  createdAt: string,
+  [string]: { id?: string }
+}
+
 export default function getMutation (
   info: GraphQLResolveInfo,
   context: Context,
@@ -15,22 +22,22 @@ export default function getMutation (
   input: {},
   subject: string
 ) {
+  const ident = getIdent()
   const stamp = new Date().toISOString()
   const node = Object.assign({}, input, { createdAt: stamp, updatedAt: stamp })
-  return getMutationFields(info, context, type, node, subject, 0)
+  return getMutationFields(context, type, node, subject, ident)
 }
 
 function getMutationFields (
-  info: GraphQLResolveInfo,
   context: Context,
   type: GraphQLObjectType,
-  input: { id?: string, updatedAt: string, createdAt: string },
+  input: NodeInput,
   subject: string,
-  count: number
+  ident: (id?: string) => string
 ) {
   const isCreate = !input.id
   let query = ''
-  if (subject.indexOf('node') !== -1) {
+  if (isCreate) {
     query += `  ${subject} <__typename> "${type.name}" .\n`
   }
   const fields = type.getFields()
@@ -38,48 +45,57 @@ function getMutationFields (
     if (key === 'id') return
     if (key === 'createdAt' && !isCreate) return
     if (typeof fields[key] === 'undefined') return
+
+    const value = input[key]
     let fieldType = unwrapNonNull(fields[key].type)
+
     if (
-      fieldType instanceof GraphQLObjectType ||
-      fieldType instanceof GraphQLList
+      fieldType instanceof GraphQLList ||
+      fieldType instanceof GraphQLObjectType
     ) {
-      let nodes = []
       if (fieldType instanceof GraphQLList) {
-        nodes = input[key]
         fieldType = unwrapNonNull(fieldType.ofType)
-      } else {
-        nodes = [input[key]]
       }
-      nodes.forEach(node => {
-        count++
-        invariant(fieldType instanceof GraphQLObjectType, 'Invalid')
-        let nodeIdent = node.id ? `<${node.id}>` : `_:node${count}`
-        let child = getMutationFields(
-          info,
-          context,
-          fieldType,
-          Object.assign({}, node, {
+      const values = Array.isArray(value) ? value : [value]
+      values
+        .map(value => {
+          invariant(typeof value === 'object', 'Input value is not object')
+          return Object.assign({}, value, {
             createdAt: input.createdAt,
             updatedAt: input.updatedAt
-          }),
-          nodeIdent,
-          count
-        )
-        query = child + query
-        query += `  ${subject} <${key}> ${nodeIdent} .\n`
-        let reverse = context.client.getReversePredicate(key)
-        if (reverse) {
-          query += `  ${nodeIdent} <${reverse}> ${subject} .\n`
-        }
-      })
+          })
+        })
+        .forEach(node => {
+          invariant(fieldType instanceof GraphQLObjectType, 'Type not object')
+          query += getNodeQuery(context, node, fieldType, ident, subject, key)
+        })
     } else {
-      const value = context.client.localizeValue(
-        input[key],
-        key,
-        context.language
-      )
-      query += `  ${subject} <${key}> ${value} .\n`
+      const locale = context.client.localizeValue(value, key, context.language)
+      query += `  ${subject} <${key}> ${locale} .\n`
     }
   })
   return query
+}
+
+function getNodeQuery (
+  context: Context,
+  input: NodeInput,
+  type: GraphQLObjectType,
+  ident: (id?: string) => string,
+  subject,
+  predicate
+) {
+  let value = ident(input.id)
+  let query = `  ${subject} <${predicate}> ${value} .\n`
+  let reverse = context.client.getReversePredicate(predicate)
+  if (reverse) {
+    query += `  ${value} <${reverse}> ${subject} .\n`
+  }
+  query += getMutationFields(context, type, input, value, ident)
+  return query
+}
+
+function getIdent () {
+  let count = 0
+  return (id?: string) => (id ? `<${id}>` : `_:node${count++}`)
 }
