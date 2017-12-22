1e7696b937982ae852413b540fbcaa818f4c5692
David Peek <mail@dpeek.com>
2017-12-22 00:29:12 +0000

    Added the worlds most dangerous cli.

---
+++ src/cli.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+require('babel-core/register')
+require('babel-polyfill')
+
+const fs = require('fs')
+const program = require('commander')
+
+const { Client } = require('./client')
+const client = new Client({ relay: false, debug: false })
+
+program
+  .arguments('<action>')
+  .option('-s, --schema <file>', 'The schema to update')
+  .action((action, options) => {
+    if (action === 'update') {
+      const schema = fs.readFileSync(options.schema).toString()
+      client.updateSchema(schema).then(() => {
+        console.log('completed')
+      })
+    } else if (action === 'drop') {
+      client.dropAll().then(() => {
+        console.log('completed')
+      })
+    }
+    console.log(action, options.schema)
+  })
+  .parse(process.argv)
